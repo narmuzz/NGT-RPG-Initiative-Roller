4916b1428461a46c5039ed7902cd3e63d46f57ca
narmuzz <42994194+narmuzz@users.noreply.github.com>
2021-05-11 16:28:49 -0300

    Planning listo

---
+++ NGT-RPG-Initiative-Roller/NGT RPG Initiative Roller/NGT RPG Initiative Roller/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NGT_RPG_Initiative_Roller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("###############################");
+            Console.WriteLine("# NGT RPG - INITIATIVE ROLLER #");
+            Console.WriteLine("###############################");
+            Console.WriteLine("FECHA");
+            Console.WriteLine("");
+        }
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# NGT-RPG-Initiative-Roller
