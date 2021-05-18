58204960fdf45749ea264d83eeb2827ea86b6420
Julián Absatz <julianabsatz@gmail.com>
2021-05-18 21:51:11 +0200

    puto

---
+++ NGT-RPG-Initiative-Roller/NGT RPG Initiative Roller/NGT RPG Initiative Roller/Entity.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NGT_RPG_Initiative_Roller
+{
+  class Entity
+  {
+    public string Name { get; set; }
+    public int Initiative { get; set; }
+  }
+}
--- NGT-RPG-Initiative-Roller/NGT RPG Initiative Roller/NGT RPG Initiative Roller/EntityClass.cs
+++ NGT-RPG-Initiative-Roller/NGT RPG Initiative Roller/NGT RPG Initiative Roller/EntityClass.cs
@@ -4,89 +4,143 @@ using System.Text;
 
 namespace NGT_RPG_Initiative_Roller
 {
-    class EntityClass
+  class EntityClass
+  {
+    public bool PlayerWin { get; set; }
+    public static List<EntityClass> PlayerList = new List<EntityClass>();
+    public static List<EntityClass> EnemyList = new List<EntityClass>();
+
+    public static void AddPlayerSide()
     {
-        public string Name { get; set; }
-        public int Initiative { get; set; }
-        public bool PlayerWin { get; set; }
+      bool continueInput = true;
 
-        public static void AddPlayerSide()
+      for (int i = 0; continueInput == true; i++)
+      {
+        Console.WriteLine("Please enter a PLAYER character name. If you don't want to add any more, please enter 0");
+
+        string temp = Console.ReadLine();
+        switch (temp)
         {
-            bool continueInput = true;
+          case "0":
+            continueInput = false;
+            break;
 
-            for (int i = 0; continueInput == true; i++)
-            {
-                Console.WriteLine("Please enter a PLAYER character name. If you don't want to add any more, please enter 0");
-
-                string temp = Console.ReadLine();
-                switch (temp)
-                {
-                    case "0":
-                    continueInput = false;
-                    break;
-
-                    default:
-                    PlayerListClass.ls.Add(new EntityClass());
-                    PlayerListClass.ls[i].Name = temp;
-                    Console.WriteLine("Please enter that character's initiative score (minimum of 1, maximum of 5)");
-                    PlayerListClass.ls[i].Initiative = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("");
-                    break;
-                }
-            }
+          default:
+            Entity player = new Entity();
+            player.Name = temp;
+            Console.WriteLine("Please enter that character's initiative score (minimum of 1, maximum of 5)");
+            player.Initiative = Convert.ToInt32(Console.ReadLine());
+            EntityClass.PlayerList.Add(player);
+            Console.WriteLine("");
+            break;
         }
-
-        public static void AddEnemySide()
-        {
-            bool continueInput = true;
-
-            for (int i = 0; continueInput == true; i++)
-            {
-                Console.WriteLine("Please enter an OPPOSING character name. If you don't want to add any more, please enter 0");
-
-                string temp = Console.ReadLine();
-                switch (temp)
-                {
-                    case "0":
-                    continueInput = false;
-                    break;
-
-                    default:
-                    EnemyListClass.ls.Add(new EntityClass());
-                    EnemyListClass.ls[i].Name = temp;
-                    Console.WriteLine("Please enter that character's initiative score (minimum of 1, maximum of 5)");
-                    EnemyListClass.ls[i].Initiative = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("");
-                    break;
-                }
-            }
-        }
-
-        public static void AddAfterStart()
-        {
-            Console.WriteLine("Please pick an option:");
-            Console.WriteLine("1 = Add a player | 2 = Add an enemy | 3 = Go back to menu");
-            int actionPicker = Convert.ToInt32(Console.ReadLine());
-            switch (actionPicker)
-            {
-                case 1:
-                    EntityClass.AddPlayerSide();
-                    EntityClass.AddAfterStart();
-                    break;
-
-                case 2:
-                    EntityClass.AddEnemySide();
-                    EntityClass.AddAfterStart();
-                    break;
-
-                case 3:
-                    MainScreenClass.PrintSides();
-
-                    Console.WriteLine("");
-
-                    InputActionPickerClass.Menu();
-                    break;
-            }
-        }
+      }
     }
+
+    public static void AddEnemySide()
+    {
+      bool continueInput = true;
+
+      for (int i = 0; continueInput == true; i++)
+      {
+        Console.WriteLine("Please enter an OPPOSING character name. If you don't want to add any more, please enter 0");
+
+        string temp = Console.ReadLine();
+        switch (temp)
+        {
+          case "0":
+            continueInput = false;
+            break;
+
+          default:
+            Entity enemy = new Entity();
+            enemy.Name = temp;
+            Console.WriteLine("Please enter that character's initiative score (minimum of 1, maximum of 5)");
+            enemy.Initiative = Convert.ToInt32(Console.ReadLine());
+            EnemyListClass.EnemyList.Add(enemy);
+            Console.WriteLine("");
+            break;
+        }
+      }
+    }
+
+    public static void AddAfterStart()
+    {
+      Console.WriteLine("Please pick an option:");
+      Console.WriteLine("1 = Add a player | 2 = Add an enemy | 3 = Go back to menu");
+      int actionPicker = Convert.ToInt32(Console.ReadLine());
+      switch (actionPicker)
+      {
+        case 1:
+          EntityClass.AddPlayerSide();
+          EntityClass.AddAfterStart();
+          break;
+
+        case 2:
+          EntityClass.AddEnemySide();
+          EntityClass.AddAfterStart();
+          break;
+
+        case 3:
+          MainScreenClass.PrintSides();
+
+          Console.WriteLine("");
+
+          InputActionPickerClass.Menu();
+          break;
+      }
+    }
+
+    public static void Ooc()
+    {
+      Console.WriteLine("Please pick an option:");
+      Console.WriteLine("1 = Mark a player as OoC | 2 = Mark an enemy as OoC | 3 = Go back to menu");
+
+      int actionPicker = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("");
+
+      switch (actionPicker)
+      {
+        case 1:
+          Console.WriteLine("Enter the index number of the desired entity");
+          for (int i = 0; i < EntityClass.PlayerList.Count; i++)
+          {
+            Console.WriteLine(i + " " + EntityClass.PlayerList[i].Name);
+          }
+
+          int pickPlayer = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("");
+
+          Console.WriteLine($"{EntityClass.PlayerList[pickPlayer].Name} is now Out of Combat");
+          EntityClass.PlayerList.RemoveAt(pickPlayer);
+
+          EntityClass.Ooc();
+          break;
+
+        case 2:
+          Console.WriteLine("Enter the index number of the desired entity");
+          for (int i = 0; i < EntityClass.EnemyList.Count; i++)
+          {
+            Console.WriteLine(i + " " + EntityClass.EnemyList[i].Name);
+          }
+
+          int pickEnemy = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("");
+
+          Console.WriteLine($"{EntityClass.EnemyList[pickEnemy].Name} is now Out of Combat");
+          EntityClass.EnemyList.RemoveAt(pickEnemy);
+
+          EntityClass.Ooc();
+          break;
+
+        case 3:
+          MainScreenClass.PrintSides();
+
+          Console.WriteLine("");
+
+          InputActionPickerClass.Menu();
+          break;
+      }
+    }
+  }
 }
