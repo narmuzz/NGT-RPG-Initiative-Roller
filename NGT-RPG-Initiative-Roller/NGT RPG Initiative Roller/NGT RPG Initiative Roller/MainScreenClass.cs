c294a95bbb04849b0f2011f5d0435926cb440244
narmuzz <42994194+narmuzz@users.noreply.github.com>
2021-05-26 04:43:40 -0300

    Minor fix

--- NGT-RPG-Initiative-Roller/NGT RPG Initiative Roller/NGT RPG Initiative Roller/MainScreenClass.cs
+++ NGT-RPG-Initiative-Roller/NGT RPG Initiative Roller/NGT RPG Initiative Roller/MainScreenClass.cs
@@ -4,65 +4,63 @@ using System.Text;
 
 namespace NGT_RPG_Initiative_Roller
 {
-  class MainScreenClass
-  {
-    public static void PrintPlayerSide()
+    class MainScreenClass
     {
-      Console.WriteLine("PLAYER SIDE:");
-      for (int i = 0; i < (EntityManager.PlayerList.Count); i++)
-      {
-        Console.Write(EntityManager.PlayerList[i].Name);
-        Console.WriteLine($" | Initiative: {EntityManager.PlayerList[i].Initiative}");
-      }
-    }
-
-    public static void PrintEnemySide()
-    {
-      Console.WriteLine("OPPOSING SIDE:");
-      for (int i = 0; i < (EntityManager.EnemyList.Count); i++)
-      {
-        Console.Write(EntityManager.EnemyList[i].Name);
-        Console.WriteLine($" | Initiative: {EntityManager.EnemyList[i].Initiative}");
-      }
-    }
-
-    public static void PrintSides()
-    {
-      Console.WriteLine("");
-      Console.WriteLine("######################################################################");
-      Console.WriteLine("");
-      MainScreenClass.PrintPlayerSide();
-      Console.WriteLine("");
-      MainScreenClass.PrintEnemySide();
-    }
-
-    public static void AskOptionWithRetries(int min, int max)
-    {
-      bool succeeded = false;
-
-      while (!succeeded)
-      {
-        try
+        public static void PrintPlayerSide()
         {
-          MainScreenClass.AskOption(min, max);
-          succeeded = true;
+            Console.WriteLine("PLAYER SIDE:");
+            for (int i = 0; i < (EntityManager.PlayerList.Count); i++)
+            {
+                Console.Write(EntityManager.PlayerList[i].Name);
+                Console.WriteLine($" | Initiative: {EntityManager.PlayerList[i].Initiative}");
+            }
         }
-        catch (FormatException ex)
+
+        public static void PrintEnemySide()
         {
-          Console.WriteLine("Option invalid: " + ex.Message);
-          Console.WriteLine("Please enter a valid option:");
+            Console.WriteLine("OPPOSING SIDE:");
+            for (int i = 0; i < (EntityManager.EnemyList.Count); i++)
+            {
+                Console.Write(EntityManager.EnemyList[i].Name);
+                Console.WriteLine($" | Initiative: {EntityManager.EnemyList[i].Initiative}");
+            }
         }
-      }
-    }
 
-    public static void AskOption(int min, int max)
-    {
-      int selectedOption = Convert.ToInt32(Console.ReadLine());
+        public static void PrintSides()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("######################################################################");
+            Console.WriteLine("");
+            MainScreenClass.PrintPlayerSide();
+            Console.WriteLine("");
+            MainScreenClass.PrintEnemySide();
+        }
 
-      if (selectedOption < min || selectedOption > max)
-      {
-        throw new FormatException("Option must be one of " + String.Join(", ", options));
-      }
+        public static int AskOptionWithRetries(int min, int max)
+        {
+            while (true)
+            {
+                try
+                {
+                    return MainScreenClass.AskOption(min, max);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Option invalid: " + ex.Message);
+                    Console.WriteLine("Please enter a valid option:");
+                }
+            }
+        }
+
+        public static int AskOption(int min, int max)
+        {
+            int selectedOption = Convert.ToInt32(Console.ReadLine());
+
+            if (selectedOption < min || selectedOption > max)
+            {
+                throw new FormatException($"Option must be between {min} and {max}");
+            }
+            return selectedOption;
+        }
     }
-  }
 }
